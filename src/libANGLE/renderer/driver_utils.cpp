73b48c33a400bc21eefe4252b90d7cb12f2a69e0
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-01-01 13:37:27 +0800

    Update driver_utils.cpp

--- src/libANGLE/renderer/driver_utils.cpp
+++ src/libANGLE/renderer/driver_utils.cpp
@@ -231,8 +231,6 @@ std::string GetVendorString(uint32_t vendorId)
             return "AMD";
         case VENDOR_ID_ARM:
             return "ARM";
-        case VENDOR_ID_Maleoon:
-            return "Maleoon";
         case VENDOR_ID_APPLE:
             return "Apple";
         case VENDOR_ID_BROADCOM:
